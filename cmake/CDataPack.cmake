498e7e3b85279abaf9764deb815d852a7672a7f1
Glistermelon <th3greatm3lon@gmail.com>
2024-12-08 15:10:13 -0600

    Provide sensible name

--- cmake/CDataPack.cmake
+++ cmake/CDataPack.cmake
@@ -3,7 +3,7 @@ function(pack_data target_name resource_dir)
         message(FATAL_ERROR "Invalid target passed to packdata: '${target_name}'")
     endif()
     add_custom_target(
-        docoolshit ALL
+        packdata_for_target ALL
         DEPENDS target_name
         COMMAND "${CMAKE_SOURCE_DIR}/packdata.exe" "$<TARGET_FILE:${target_name}>" ${resource_dir}
         COMMENT "Packing data for target ${target_name}"
