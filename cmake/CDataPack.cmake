7dcdd3ebb2b66b8b8227ca8ed57948fb8a443505
Glistermelon <th3greatm3lon@gmail.com>
2024-12-08 15:09:07 -0600

    Remove old cmake implementation

--- cmake/CDataPack.cmake
+++ cmake/CDataPack.cmake
@@ -2,11 +2,6 @@ function(pack_data target_name resource_dir)
     if (NOT TARGET ${target_name})
         message(FATAL_ERROR "Invalid target passed to packdata: '${target_name}'")
     endif()
-    # set(files_joined)
-    # math(EXPR argc_m1 "${ARGC} - 1")
-    # foreach(i RANGE 1 ${argc_m1})
-    #     list(APPEND files_joined "${ARGV${i}}")
-    # endforeach()
     add_custom_target(
         docoolshit ALL
         DEPENDS target_name
